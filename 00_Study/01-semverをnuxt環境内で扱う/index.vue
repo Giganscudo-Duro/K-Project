2832e0a857ceb6f11346f29864a34ec4798844e7
Giganscudo-Duro <Giganscudo_Duro@outlook.com>
2023-02-14 03:51:07 +0900

    修正

--- 00_Study/01-semverをnuxt環境内で扱う/index.vue
+++ 00_Study/01-semverをnuxt環境内で扱う/index.vue
@@ -6,34 +6,33 @@
   </div>
 </template>
 
-
 <script lang="ts">
-
 const compareVersions = require('compare-versions');
 
-console.log("vheckAgentVersion = " + checkAgentVersion('010402', '010400'))
-console.log("vheckAgentVersion = " + checkAgentVersion('010399', '010400'))
-console.log("vheckAgentVersion = " + checkAgentVersion('01039', '010400'))
+// 動作確認用
+console.log("DEBUG: checkAgentVersion = " + checkAgentVersion('010402', '010400'))
+console.log("DEBUG: checkAgentVersion = " + checkAgentVersion('010399', '010400'))
+console.log("DEBUG: checkAgentVersion = " + checkAgentVersion('01039', '010400'))
 
+
+// 規定フォーマット（XXYYZZ）の文字列を受け取り、比較結果をもとにbooleanを返す
 function checkAgentVersion(targetversion: string, funcversion:string): boolean {
   let targetsemver = convertVersion(targetversion)
   let funcsemver = convertVersion(funcversion)
-  console.log('targetsemver = ' + targetsemver)
-  console.log('funcsemver = ' + funcsemver)
-
   if ( targetsemver === '' || funcsemver === '' ) {
     // targetsemver, funcsemver のどちらかが、semverへの変換に失敗
-    console.log('semverへの変換に失敗')
     return false
   } else if (compareVersions.compare(targetsemver, funcsemver, '>=')) {
     // 指定したバージョンの条件を満たす場合
     return true
   } else {
-    console.log('targetversion が条件を満たしていない')
+    // 指定したバージョンの条件を満たさない場合
     return false
   }
 }
 
+
+// 規定フォーマット（XXYYZZ）の文字列を受け取り、semver 形式の文字列変換する
 function convertVersion(version: string): string {
   if (version.length !== 6) {
     // 版数が既定の文字数ではない
@@ -54,12 +53,9 @@ function convertVersion(version: string): string {
     // patch番号を数値化できない
     return ''
   }
+  // 適切に変換できた場合は、全てをドットで連結し、semver形式の文字列を作成する
   return majour + '.' + minor + '.' + patch
 }
-
-
-
-
 
 
 </script>
