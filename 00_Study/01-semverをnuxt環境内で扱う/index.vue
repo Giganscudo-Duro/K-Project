ebca0fa4114c1863a0ccc8cb50be2f71e23cee59
Giganscudo-Duro <Giganscudo_Duro@outlook.com>
2023-02-14 03:29:52 +0900

    機能説明などを微修正

--- 00_Study/01-semverをnuxt環境内で扱う/index.vue
+++ 00_Study/01-semverをnuxt環境内で扱う/index.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div class="container">
     <div>
@@ -8,52 +7,53 @@
 </template>
 
 
-
-
 <script lang="ts">
 
 const compareVersions = require('compare-versions');
 
-console.log("compareVersion = " + compareVersions.compare('1.1.2', '1.1', '>='))
-
 console.log("vheckAgentVersion = " + checkAgentVersion('010402', '010400'))
 console.log("vheckAgentVersion = " + checkAgentVersion('010399', '010400'))
-console.log("convertVersion = " + convertVersion('123456'))
+console.log("vheckAgentVersion = " + checkAgentVersion('01039', '010400'))
 
 function checkAgentVersion(targetversion: string, funcversion:string): boolean {
-
   let targetsemver = convertVersion(targetversion)
   let funcsemver = convertVersion(funcversion)
   console.log('targetsemver = ' + targetsemver)
-  console.log('funcsemver = ' + targetsemver)
+  console.log('funcsemver = ' + funcsemver)
 
-  if ( targetsemver !== '' || funcsemver !== '' ) {
+  if ( targetsemver === '' || funcsemver === '' ) {
+    // targetsemver, funcsemver のどちらかが、semverへの変換に失敗
+    console.log('semverへの変換に失敗')
     return false
   } else if (compareVersions.compare(targetsemver, funcsemver, '>=')) {
+    // 指定したバージョンの条件を満たす場合
     return true
   } else {
+    console.log('targetversion が条件を満たしていない')
     return false
   }
 }
 
 function convertVersion(version: string): string {
   if (version.length !== 6) {
+    // 版数が既定の文字数ではない
     return ''
   }
-
   let majour = Number(version.substring(0,2))
   if (isNaN(majour)) {
+    // majour番号を数値化できない
     return ''
   }
   let minor = Number(version.substring(2,4))
   if (isNaN(minor)) {
+    // minor番号を数値化できない
     return ''
   }
   let patch = Number(version.substring(4,6))
   if (isNaN(patch)) {
+    // patch番号を数値化できない
     return ''
   }
-  console.log('semver = ' + majour + '.' + minor + '.' + patch)
   return majour + '.' + minor + '.' + patch
 }
 
